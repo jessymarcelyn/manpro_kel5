a4abbe23a3c92c786383e886309969d6011ddb59
Martuiil <martui@outlook.co.id>
2024-04-20 14:55:27 +0700

    Navbar

---
+++ app/src/main/java/manpro/kel5/proyek_manpro/list_view_bookmarks.kt
@@ -0,0 +1,20 @@
+package manpro.kel5.proyek_manpro
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class list_view_bookmarks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_list_view_bookmarks)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
