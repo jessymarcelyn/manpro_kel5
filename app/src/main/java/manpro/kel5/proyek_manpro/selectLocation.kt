31cd1d049a709d9b3d5df0b7a24d073052bbdae1
Martuiil <martui@outlook.co.id>
2024-04-29 20:52:55 +0700

    ListView Home, SelectRoute, SelectLocation

--- app/src/main/java/manpro/kel5/proyek_manpro/selectLocation.kt
+++ app/src/main/java/manpro/kel5/proyek_manpro/selectLocation.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class list_view_bookmarks : AppCompatActivity() {
+class selectLocation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_list_view_bookmarks)
+        setContentView(R.layout.activity_select_location)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
