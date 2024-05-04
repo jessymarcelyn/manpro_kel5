a864259965114ba04e48a257ce72e38abe95c7fd
Martuiil <martui@outlook.co.id>
2024-05-04 20:06:28 +0700

    SelectRute error

--- app/src/main/java/manpro/kel5/proyek_manpro/SelectRute.kt
+++ app/src/main/java/manpro/kel5/proyek_manpro/SelectRute.kt
@@ -11,24 +11,24 @@ class SelectRute : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selectrute)
 
-        val recyclerView: RecyclerView = findViewById(R.id.routeSelect)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-        val tvRuteJalan: TextView = findViewById(R.id.tv_rutejalan)
-        val routeIds: ArrayList<String>? = intent.getStringArrayListExtra("routeIds")
-        println("Isi intent: $routeIds")
-        if (routeIds != null) {
-            val routeStringBuilder = StringBuilder()
-            routeStringBuilder.append("Rute:\n")
-            for ((index, routeId) in routeIds.withIndex()) {
-                routeStringBuilder.append("Rute ${index + 1}: $routeId -> $routeId\n")
-            }
-
-            tvRuteJalan.text = routeStringBuilder.toString()
-
-            val routeIdsList: List<List<String>> = listOf(routeIds)
-            val adapter = RouteAdapter(routeIdsList)
-            recyclerView.adapter = adapter
-        }
+//        val recyclerView: RecyclerView = findViewById(R.id.routeSelect)
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+//
+//        val tvRuteJalan: TextView = findViewById(R.id.tv_rutejalan)
+//        val routeIds: ArrayList<String>? = intent.getStringArrayListExtra("routeIds")
+//        println("Isi intent: $routeIds")
+//        if (routeIds != null) {
+//            val routeStringBuilder = StringBuilder()
+//            routeStringBuilder.append("Rute:\n")
+//            for ((index, routeId) in routeIds.withIndex()) {
+//                routeStringBuilder.append("Rute ${index + 1}: $routeId -> $routeId\n")
+//            }
+//
+//            tvRuteJalan.text = routeStringBuilder.toString()
+//
+//            val routeIdsList: List<List<String>> = listOf(routeIds)
+//            val adapter = RouteAdapter(routeIdsList)
+//            recyclerView.adapter = adapter
+//        }
     }
 }
